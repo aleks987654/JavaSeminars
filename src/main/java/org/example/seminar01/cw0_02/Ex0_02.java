a86e31a0f8ae5e86ea7dc06b9b50972b2b44d20d
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-07-14 21:11:05 +0700

    done javaclass Ex0_02.java

--- src/main/java/org/example/seminar01/cw0_02/Ex0_02.java
+++ src/main/java/org/example/seminar01/cw0_02/Ex0_02.java
@@ -1,4 +1,35 @@
 package org.example.seminar01.cw0_02;
 
+import java.util.Scanner;
+
 public class Ex0_02 {
+    public static void main(String[] args) {
+        /** Дана последовательность N целых чисел.
+         *  Найти количество положительных чисел,
+         *  после которых следует отрицательное число.
+         */
+
+        System.out.println("Input count of numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = getK(scanner, n);
+        System.out.println("k = " + k);
+
+    }
+
+    private static int getK(Scanner scanner, int n) {
+        int k = 0;
+        System.out.println("Input number 1: ");
+        int a = scanner.nextInt();
+        int b;
+        for (int i = 1; i < n; i++) {
+            System.out.println(String.format("Input number %d: ", i+1));
+            b = scanner.nextInt();
+            if (a > 0 && b < 0) {
+                k++;
+            }
+            a = b;
+        }
+        return k;
+    }
 }
