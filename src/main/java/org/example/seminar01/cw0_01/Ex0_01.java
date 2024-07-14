db5c18de81d779c31188f723c0404075b65f7488
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-07-14 20:28:59 +0700

    add cw0_01 examp1 of seminar01

---
+++ src/main/java/org/example/seminar01/cw0_01/Ex0_01.java
@@ -0,0 +1,33 @@
+package org.example.seminar01.cw0_01;
+
+import java.util.Scanner;
+
+public class Ex0_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, occupants-pirates!");
+
+        System.out.println("Input number: ");
+        Scanner scanner = new Scanner(System.in);//TODO : переименовать
+        int n = scanner.nextInt();
+        int result = getPrMinusSum(n);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * @apiNote description
+     * @param n input number
+     * @return product minus sum
+     */
+
+    private static int getPrMinusSum(int n) {
+        int sum = 0;
+        int pr = 1;
+        while (n != 0) {
+            sum += n % 10;
+            pr *= n % 10;
+            n /= 10;
+        }
+        return pr - sum;
+    }
+
+}
