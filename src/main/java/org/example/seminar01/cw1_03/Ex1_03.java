e04a20166b205e1b5841459a6fedb4b1d8f83604
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-07-14 21:44:44 +0700

    add javaclass Ex1_03.java

---
+++ src/main/java/org/example/seminar01/cw1_03/Ex1_03.java
@@ -0,0 +1,18 @@
+package org.example.seminar01.cw1_03;
+
+import java.util.Scanner;
+
+/**
+ * Дана строка. Поменять местами её половины.
+ */
+
+public class Ex1_03 {
+    public static void main(String[] args) {
+        String str = "qweasd";
+        String result = str.substring(str.length()/2) +
+                        str.substring(0, str.length()/2);
+        System.out.println("result = " + result);
+
+    }
+
+}
